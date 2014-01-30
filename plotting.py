40bb46be6493fca3b453593553126b68418930dc
Ryan Keisler <rkeisler@gmail.com>
2014-01-30 14:31:29 -0800

    color=logarithm(redshift)

--- plotting.py
+++ plotting.py
@@ -1,18 +1,20 @@
 import matplotlib.pylab as pl
+import numpy as np
 
 def plot_photodata(d):
-    n2pl=1e4; s=5; cmap=pl.cm.jet; fs=14;
+    n2pl=10000; s=5; cmap=pl.cm.RdBu_r; fs=14;
+    colors = np.log((d.redshift)[0:n2pl])
     pl.figure(figsize=(10,4))
     pl.subplot(1,2,1)
     pl.scatter((d.g-d.r)[0:n2pl], (d.i)[0:n2pl], 
-               c=(d.redshift)[0:n2pl], s=s, cmap=cmap, edgecolors='none')
+               c=colors, s=s, cmap=cmap, edgecolors='none')
     pl.xlim(-0.5,2)
     pl.ylim(13,21)
     pl.xlabel('Color [g-r]',fontsize=fs)
     pl.ylabel('Magnitude [i]',fontsize=fs)
     pl.subplot(1,2,2)
     pl.scatter((d.r-d.i)[0:n2pl], (d.i-d.z)[0:n2pl], 
-        c=(d.redshift)[0:n2pl], s=s, cmap=cmap, edgecolors='none')
+        c=colors, s=s, cmap=cmap, edgecolors='none')
     pl.xlim(-0.1,1.1)
     pl.ylim(-.2, .8)
     pl.xlabel('Color [r-i]',fontsize=fs)
